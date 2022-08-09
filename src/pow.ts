f09ff9bc1a82ea54b887555d3849ffe3642cff49
qpounodb <qpundb+github@ya.ru>
2022-08-09 17:12:33 +0300

    feat: solved task Pow

--- src/pow.ts
+++ src/pow.ts
@@ -1,3 +1,25 @@
-const pow = () => {};
+const assertNumber = (x: unknown): void => {
+  if (typeof x !== 'number') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+};
 
-export default pow;
+type PowOnFixedBase = (exponent: number) => number;
+
+const curriedPow = (base: number): PowOnFixedBase => {
+  assertNumber(base);
+  return (exponent) => {
+    assertNumber(exponent);
+    return base ** exponent;
+  };
+};
+
+export default function pow(base: number): PowOnFixedBase;
+export default function pow(base: number, exponent: number): number;
+export default function pow(
+  base: number,
+  exponent?: number
+): number | PowOnFixedBase {
+  const powOnFixedBase = curriedPow(base);
+  return exponent === undefined ? powOnFixedBase : powOnFixedBase(exponent);
+}
