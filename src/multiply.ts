09a04cca4dc1a1f011051a1906037f29f3eece47
qpounodb <qpundb+github@ya.ru>
2022-08-09 20:33:37 +0300

    feat: solved task multiply

--- src/multiply.ts
+++ src/multiply.ts
@@ -1,3 +1,15 @@
-const multiply = () => {};
+import { makeAssertNumberFunc } from './lib/assert';
+
+const assertNumber = makeAssertNumberFunc('INVALID_ARGUMENT');
+
+type MultiplyByFixedNum = (b: number) => number;
+
+const multiply = (a: number): MultiplyByFixedNum => {
+  assertNumber(a);
+  return (b) => {
+    assertNumber(b);
+    return a * b;
+  };
+};
 
 export default multiply;
