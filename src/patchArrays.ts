1e043bc69622c7d5d043f5fbaf98524968d2920b
qpounodb <qpundb+github@ya.ru>
2022-08-09 20:23:26 +0300

    feat: solved task "patch Arrays"

--- src/patchArrays.ts
+++ src/patchArrays.ts
@@ -1,5 +1,26 @@
-// @ts-nocheck
+import { makeAssertNumberFunc } from './lib/assert';
 
-const patchArrays = (): void => {};
+const assertNumber = makeAssertNumberFunc('INVALID_ARGUMENT');
+
+const patchArrays = (): void => {
+  Object.assign(Array.prototype, {
+    count<T>(this: T[]): number {
+      return this.length;
+    },
+
+    insert<T>(this: T[], index: number, value: T): T[] {
+      this.splice(assertNumber(index) < 0 ? 0 : index, 0, value);
+      return this;
+    },
+
+    remove<T>(this: T[], value: T): T[] {
+      const index = this.indexOf(value);
+      if (index > -1) {
+        this.splice(index, 1);
+      }
+      return this;
+    },
+  });
+};
 
 export default patchArrays;
