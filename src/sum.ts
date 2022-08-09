4db7182fb261fa449189a7d2e9ebad74619d9f84
qpounodb <qpundb+github@ya.ru>
2022-08-09 15:54:55 +0300

    feat: solved task "Sum"

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,11 @@
-const sum = () => {};
+const sum = (...nums: number[]): number => {
+  if (nums.length < 2) {
+    throw new Error('INVALID_ARGUMENTS_COUNT');
+  }
+  if (nums.some((x) => typeof x !== 'number')) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+  return nums.reduce((acc, x) => acc + x, 0);
+};
 
 export default sum;
