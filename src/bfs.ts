1939b317ef06bfe161d8940b4e2b7172611b9cc6
qpounodb <qpundb+github@ya.ru>
2022-08-10 10:32:59 +0300

    refactor: move check on undefined

--- src/bfs.ts
+++ src/bfs.ts
@@ -8,20 +8,15 @@ type Level = Node[];
 
 const bfs = (tree: Tree): Node[] => {
   const root = Object.keys(assertObject(tree)).at(0);
-  return Array.from(genBfs(tree, root)).flat();
+  return root === undefined ? [] : Array.from(genBfs(tree, root)).flat();
 };
 
 export default bfs;
 
-function* genBfs(tree: Tree, root?: Node): Generator<Level, Level> {
-  if (root === undefined) {
-    return [];
-  }
-
+function* genBfs(tree: Tree, root: Node): Generator<Level> {
   let level = [root];
   while (level.length > 0) {
     yield level;
     level = level.flatMap((node) => tree[node]);
   }
-  return level;
 }
